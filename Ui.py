b3a92d9536cd642de94a3741d75f7d6cc58cfcab
Jeab <moonmoon2917@gmail.com>
2025-10-18 10:45:38 +0700

    init repo

--- Ui.py
+++ Ui.py
@@ -8,45 +8,129 @@ except:
 import maya.OpenMayaUI as omui
 import os
 
-ICON_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'icon'))
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+ICON_PATH = os.path.join(BASE_DIR, 'icon')
+FONT_PATH = os.path.join(BASE_DIR, 'font', 'Adobe Dia.ttf')
+FONT_ICON_PATH = os.path.join(BASE_DIR, 'font', 'pixelfont_6.ttf')
+IMAGE_PATH = BASE_DIR
 
 class MakeControlCurve(QtWidgets.QDialog):
 	def __init__(self, parent=None):
 		super().__init__(parent)
 
-		self.resize(300, 300)
-		self.setWindowTitle('Make Your Own Super Cute Control Curve')
+		self.resize(533, 600)
+		self.setWindowTitle('☆*: .｡.  Cute Control Curve  .｡.:*☆')
 		self.main_layout = QtWidgets.QVBoxLayout()
 		self.setLayout(self.main_layout)
+		
+		# ภาพพื้นหลัง
+		image_path = IMAGE_PATH.replace("\\", "/") + "/image/sky.jpg"
+		self.setStyleSheet(f'''
+				QDialog {{
+					background-image: url("{image_path}");
+					background-repeat: no-repeat;
+					background-position: center;
+					background-attachment: fixed;
+					background-size: cover;
+					background-color: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 #6E8CFB, stop:1 #3C467B);
+				}}
+			''')
 
-		self.primitive_listWidget = QtWidgets.QListWidget()
-		self.primitive_listWidget.setIconSize(QtCore.QSize(60, 60))
-		self.primitive_listWidget.setSpacing(8)
-		self.primitive_listWidget.setViewMode(QtWidgets.QListView.IconMode)
-		self.primitive_listWidget.setMovement(QtWidgets.QListView.Static)
-		self.primitive_listWidget.setResizeMode(QtWidgets.QListView.Adjust)
-		self.main_layout.addWidget(self.primitive_listWidget)
+		# ภาพหัวข้อ
+		self.imageLabel =QtWidgets.QLabel()
+		self.imagePixmap = QtGui.QPixmap(f"{IMAGE_PATH}/image/moon.jpg")
+		scaled_pixmap = self.imagePixmap.scaled(
+			QtCore.QSize(500,150),
+			QtCore.Qt.KeepAspectRatio,
+			QtCore.Qt.SmoothTransformation
+		)
+		self.imageLabel.setPixmap(scaled_pixmap)
+		self.imageLabel.setAlignment(QtCore.Qt.AlignCenter)
+		self.main_layout.addWidget(self.imageLabel)
 
-		self.button_layout = QtWidgets.QHBoxLayout()
-		self.main_layout.addLayout(self.button_layout)
-		self.create_button = QtWidgets.QPushButton('Create')
-		#self.create_button.clicked.connect()
-		self.cancel_button = QtWidgets.QPushButton('Canael')
-		self.cancel_button.clicked.connect(self.close)
+		# โหลดฟ้อนต์
+		font_id = QtGui.QFontDatabase.addApplicationFont(FONT_PATH)
+		if font_id != -1:
+			font_family = QtGui.QFontDatabase.applicationFontFamilies(font_id)[0]
+			print(f"Loaded font: {font_family}")
+		else:
+			font_family = 'Papyrus'  # ฟ้อนต์สำรองถ้าโหลดไม่สำเร็จ
+			print("Failed to load custom font, using Arial instead.")
 
-		self.button_layout.addWidget(self.create_button)
-		self.button_layout.addWidget(self.cancel_button)
+		self.Jeab_listW = QtWidgets.QListWidget()
+		self.Jeab_listW.setIconSize(QtCore.QSize(60, 60))
+		self.Jeab_listW.setSpacing(8)
+		self.Jeab_listW.setViewMode(QtWidgets.QListView.IconMode)
+		self.Jeab_listW.setMovement(QtWidgets.QListView.Static)
+		self.Jeab_listW.setResizeMode(QtWidgets.QListView.Adjust)
+		self.Jeab_listW.setStyleSheet("background: rgba(5, 0, 76, 0.31); border: none;")
+		self.main_layout.addWidget(self.Jeab_listW)
+
+		self.jeab_ButLay = QtWidgets.QHBoxLayout()
+		self.main_layout.addLayout(self.jeab_ButLay)
+		self.Jeab_CreateBut = QtWidgets.QPushButton('🌕 Create')
+		#self.Jeab_CreateBut.clicked.connect()
+		self.Jeab_CreateBut.setFont(QtGui.QFont(font_family, 16, QtGui.QFont.Bold))
+		self.Jeab_CreateBut.setStyleSheet(
+			'''
+				QPushButton {
+				background-color: #000B58;
+				color: #FEB21A;
+				border-radius: 10px;
+				padding: 5px; 
+				font-size: 35px;
+				font-weight: bold;
+				}
+				QPushButton:hover {
+					background-color: #6E8CFB;
+				}
+				QPushButton:pressed {
+					background-color: navy;
+				}
+			'''
+			)
+		self.Jeab_CancelBut = QtWidgets.QPushButton('Cancel 🌙')
+		self.Jeab_CancelBut.clicked.connect(self.close)
+		self.Jeab_CancelBut.setFont(QtGui.QFont(font_family, 16, QtGui.QFont.Bold))
+		self.Jeab_CancelBut.setStyleSheet(
+			'''
+				QPushButton {
+				background-color: #000B58;
+				color: #FEB21A;
+				border-radius: 10px;
+				padding: 5px; 
+				font-size: 35px;
+				font-weight: bold;
+				}
+				QPushButton:hover {
+					background-color: #6E8CFB;
+				}
+				QPushButton:pressed {
+					background-color: navy;
+				}
+			'''
+			)
+
+		self.jeab_ButLay.addWidget(self.Jeab_CreateBut)
+		self.jeab_ButLay.addWidget(self.Jeab_CancelBut)
 
 		self.initIconWidgets()
 
 	def initIconWidgets(self):
-		prims = ['foot_crv', 'move_cru', 'root_crv']
+		prims = ['GF1', 'GF2', 'GF3', 'GF4', 'GF5', 'GF6', 'GF7', 
+		'GF8', 'GF9', 'GF10', 'GF11', 'GF12', 'GF13', 'GF14', 
+		'GF15', 'GF16', 'GF17', 'GF18', 'GF19', 'GF20', 'GF21', 
+		'GF22', 'GF23', 'GF24', 'GF25', 'GF26', 'GF27', 'GF28']
+		
+		font_id2 = QtGui.QFontDatabase.addApplicationFont(FONT_ICON_PATH)
+		families = QtGui.QFontDatabase.applicationFontFamilies(font_id2)[0]
+		item_font = QtGui.QFont(families, 16, QtGui.QFont.Bold)
+		
 		for prim in prims:
 			item = QtWidgets.QListWidgetItem(prim)
 			item.setIcon(QtGui.QIcon(os.path.join(ICON_PATH, f'{prim}.png')))
-			self.primitive_listWidget.addItem(item)
-
-
+			item.setFont(item_font)
+			self.Jeab_listW.addItem(item)
 
 def run():
 	global ui
